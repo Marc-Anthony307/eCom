16b81436a4e138d281a923f7286850c8e5840196
Marc-Anthony307 <112972669+Marc-Anthony307@users.noreply.github.com>
2024-02-02 14:23:32 -0500

    Added addition methods and stuff

--- index.php
+++ index.php
@@ -20,4 +20,11 @@ $url = $_GET['url'];
 $parsedUrl = parse($url);
 var_dump($parsedUrl);
 
+
+[$fn, $op1, $op2] = $parsedUrl;
+
+//it is dangerous in PHP to write () after a string not inside quotes
+echo "the result of $op1 + $op2 is ", $fn($op1,$op2);       
+
+
 ?>
