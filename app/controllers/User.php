f97fdf91a02aeb792175a5fc0f6205f2edde6b5c
Marc-Anthony307 <112972669+Marc-Anthony307@users.noreply.github.com>
2024-02-29 09:48:23 -0500

    Fixed

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -7,24 +7,25 @@ class User extends \app\core\Controller
     function login()
     {
         if (($_SERVER['REQUEST_METHOD'] === 'POST')) { //"===" checking if they are equal and of the same data type
-            //log the user in if the password is right
-            //get the user from the database
-            $username = $_POST['username'];
-            $user = new \app\models\User();
-            $user = $user->get($username);
-            //check the password against the hash
-            $password = $_POST['password'];
-            if ($user && password_verify($password, $user->password_hash)) {
-                //remember that this is the user logging in...
-                $_SESSION['user_id'] = $user->user_id;
+            //log the user in... if the password is right
+			//get the user from the database
+			$username = $_POST['username'];
+			$user = new \app\models\User();
+			$user = $user->get($username);
+			//check the password against the hash
+			$password = $_POST['password'];
+			if($user && $user->active && password_verify($password, $user->password_hash)){
+				//remember that this is the user logging in...
+				$_SESSION['user_id'] = $user->user_id;
 
-                header('location:/User/securePlace');
-            }
-
-        } else {
-            $this->view('User/login');
-        }
-    }
+				header('location:/User/securePlace');
+			}else{
+				header('location:/User/login');
+			}
+		}else{
+			$this->view('User/login');
+		}
+	}
 
     function logout()
     {
@@ -42,62 +43,61 @@ class User extends \app\core\Controller
         }
         echo "You are safe. You are in a secure location.";
     }
-}
 
 function register()
 {
-    //display the form and process the registration
-    if (($_SERVER['REQUEST_METHOD'] === 'POST')) { //"===" checking if they are equal and of the same data type
-        //getting the user input and place it in an object
-        //create a new User
-        $username = $_POST['username'];
-        $user = new \app\models\User();
-        $user = $user->get($username);
-
-        //populate the User
-        $password = $_POST['password'];
-        if ($user ** password_verify($password, $user->password_hash)) {     //FIXXXXXXXXXXXXXXXXXXXXXXXXXX
-            $_SESSION['user_id'] = $user->user_id;
-        }
-        $user->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-        //insert the User
-        $user->insert();
-
-        //redirect to a good place
-        header('location:/User/login');
-    } else {
-        $this->view('User/registration');
-    }
-}
+   	//display the form and process the registration
+		if($_SERVER['REQUEST_METHOD'] === 'POST'){
+			//getting the user input and place it in an object
+			//create the new User
+			$user = new \app\models\User();
+			//populate the User
+			$user->username = $_POST['username'];
+			$user->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+			//insert the user
+			$user->insert();
+			//redirect to a good place
+			header('location:/User/login');
+		}else{
+			$this->view('User/registration');
+		}
+	}
 
 //update our own user record (only if I am logged in)
-function update()
-{
-
-    if (!isset($_SESSION['user_id'])) {
+function update(){
+    if(!isset($_SESSION['user_id'])){
         header('location:/User/login');
         return;
     }
 
     $user = new \app\models\User();
-    $user = $user->getById($_SESSION['user_id']); //TODO
+    $user = $user->getById($_SESSION['user_id']);
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
         //process the update
         $user->username = $_POST['username'];
         //change the password only if one is sent
         $password = $_POST['password'];
-        if (!empty($password)) { //should be false if ''
+        if(!empty($password)){//should be false if ''
             $user->password_hash = password_hash($password, PASSWORD_DEFAULT);
-        
-        }
-        //otherwise remains as it was
+        }//otherwise remains as it was
         $user->update();
         header('location:/User/update');
-    } else {
-        $this->view('User/update');
+    }else{
+        $this->view('User/update', $user);
+    }
+}
+
+function delete(){
+    if(!isset($_SESSION['user_id'])){
+        header('location:/User/login');
+        return;
     }
 
+    $user = new \app\models\User();
+    $user = $user->getById($_SESSION['user_id']);
+    $user->delete();
+    header('location:/User/logout');
 }
+
 }
